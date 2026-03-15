2a5acd1017023ba555cadc3644c9e55715c9bb79
Angel21777 <tixomirovangel@gmail.com>
2026-03-15 15:25:29 +0200

    Add files via upload

---
+++ zad4a/zad2.cpp
@@ -0,0 +1,15 @@
+// zad2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    float x = 0.5;
+	float result;
+	result = sqrt (pow(x,2) - (6 * x) + 9);
+	printf("result=%f", result);
+}
+
+
