fc0e781f1fc465137c3d3e6db22ad8d5e38928ad
Angel21777 <tixomirovangel@gmail.com>
2026-03-15 15:24:41 +0200

    Add files via upload

---
+++ zad4b/zad4b.cpp
@@ -0,0 +1,15 @@
+// zad4b.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int b = 7;
+    double result;
+    result = (sqrt(b) - sqrt(5)) * (sqrt(b) + sqrt(5));
+    printf("result = % lf", result);
+}
+
+
